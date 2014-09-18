dc5ca4e511766c06813dde17716b83a1137a7acd
AlisamfP <eatliightning@gmail.com>
2014-09-18 16:41:07 -0700

    IT WORKS

--- gifbot.js
+++ gifbot.js
@@ -1,8 +1,9 @@
 var irc = require('irc');
-var _   = require('lodash'); 
+var _   = require('lodash');
+var request = require('request');
 
 var testRoom = '#testingbots';
-var publicBetakey = 'dc6zaTOxFJmzC';
+var publicBetakey = '&limit=1&api_key=dc6zaTOxFJmzC';
 
 var bot = new irc.Client('irc.freenode.net', 'gif_bot', {channels: [testRoom]});
 
@@ -13,17 +14,30 @@ function init(){
         bot.say(testRoom, 'I AM A BOT THAT WILL GIVE YOU GIFS')
         console.log('connected to ' + testRoom);
     });
-    findGifs();
-}
-
-function findGifs(){
     bot.addListener('message', function (from, to, text){
         if(_.contains(text, 'gif me')){
             text = text.split(/[, ]+/).slice(text.split(/[, ]+/).indexOf('me'));
             text.shift();
-            bot.say(to, text);
+            console.log(text.join('+'));
+            findGif(text.join('+'));
+        }
+    });
+}
+
+function findGif(search){
+    var url = 'http://api.giphy.com/v1/gifs/search?q=' + search + publicBetakey
+    request(url, function (err, res, body){
+        if(!err && res.statusCode == 200 && _.isEmpty(JSON.parse(body).data)!=true){
+            body = JSON.parse(body);
+            console.log(body);
+            bot.say(testRoom, body.data[0].bitly_gif_url);
+        }
+        else{
+            bot.say(testRoom, 'NOPE!');
         }
     })
 }
 
 init();
+
+
