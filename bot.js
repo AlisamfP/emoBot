f66b6bd0152e4f78a836f20707165c0b71805916
AlisamfP <eatliightning@gmail.com>
2014-09-17 17:05:19 -0700

    added ability to get list of tags

--- bot.js
+++ bot.js
@@ -2,11 +2,11 @@ var irc = require('irc');
 var emoji = require('emo/lib/emotes');
 var _ = require('lodash');
 
-var bot = new irc.Client('irc.freenode.net', 'emobot',{channels: ['#phxtech']});
+var bot = new irc.Client('irc.freenode.net', 'emobot',{channels: ['#testingbots']});
 
 bot.addListener('message', function (from, to, text){
-   var message = text.split(/[, ]+/);
-    if (_.contains(message, 'emoji')){
+   var message = text.split(/[, ]+/).slice(text.split(/[, ]+/).indexOf('emotibot'));
+    if (_.contains(message, 'emotibot')){
         message.shift();
         if(_.isEmpty(message)){
             console.log('empty array');
@@ -16,6 +16,11 @@ bot.addListener('message', function (from, to, text){
             message = message.join(' ');
             if(_.contains(emoji.getTags(), message)){
                 bot.say(to, emoji.select(message).string);
+            }
+            else if(_.contains(message, 'tags')){
+                var tags = emoji.getTags();
+                tags = tags.join(', ');
+                bot.say(from, tags);
             }
             else{
                 bot.say(to, 'NOPE');
