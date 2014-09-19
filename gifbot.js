31ac4b83349c4981e15fbd55e2c6eeef7adad2b3
AlisamfP <eatliightning@gmail.com>
2014-09-19 13:10:58 -0700

    randomizes gif

--- gifbot.js
+++ gifbot.js
@@ -2,8 +2,8 @@ var irc = require('irc');
 var _   = require('lodash');
 var request = require('request');
 
-var testRoom = '#phxtech';
-var publicBetakey = '&limit=1&api_key=dc6zaTOxFJmzC';
+var testRoom = '#testingbots';
+var publicBetakey = '&api_key=dc6zaTOxFJmzC';
 
 var bot = new irc.Client('irc.freenode.net', 'gif_bot', {channels: [testRoom]});
 
@@ -18,7 +18,6 @@ function init(){
         if(_.contains(text, 'gif me')){
             text = text.split(/[, ]+/).slice(text.split(/[, ]+/).indexOf('me'));
             text.shift();
-            console.log(text.join('+'));
             findGif(text.join('+'));
         }
     });
@@ -30,12 +29,20 @@ function findGif(search){
         if(!err && res.statusCode == 200 && _.isEmpty(JSON.parse(body).data)!=true){
             body = JSON.parse(body);
             console.log(body);
-            bot.say(testRoom, body.data[0].images.original.url);
+            bot.say(testRoom, body.data[randomize(body)].images.original.url);
         }
         else{
             bot.say(testRoom, 'NOPE!');
         }
     })
+}
+function randomize(data){
+    var count = (data.pagination.count) - 1;
+    if (count >= 0){
+        var random = _.random(0, count);
+        console.log(random);
+        return random;
+    }
 }
 
 init();
