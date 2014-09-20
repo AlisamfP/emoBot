c52c939dc040af656057d9b4b7487d5d718702b2
AlisamfP <eatliightning@gmail.com>
2014-09-19 17:53:54 -0700

    using random api call instead of randomizing results

--- gifbot.js
+++ gifbot.js
@@ -3,7 +3,7 @@ var _   = require('lodash');
 var request = require('request');
 
 var testRoom = '#testingbots';
-var publicBetakey = '&api_key=dc6zaTOxFJmzC';
+var publicBetakey = '&limit=1&api_key=dc6zaTOxFJmzC';
 
 var bot = new irc.Client('irc.freenode.net', 'gif_bot', {channels: [testRoom]});
 
@@ -11,39 +11,46 @@ function init(){
     bot.addListener('error', function(error){ console.log('error: ' + error);} );
     bot.addListener('register', function(message){ console.log('registered: ' + message); });
     bot.join(testRoom, function(){
-        bot.say(testRoom, 'I AM A BOT THAT WILL GIVE YOU GIFS')
+        bot.say(testRoom, 'Type gif me <what you want a gif of>')
         console.log('connected to ' + testRoom);
     });
     bot.addListener('message', function (from, to, text){
+        text = text.toLowerCase();
         if(_.contains(text, 'gif me')){
             text = text.split(/[, ]+/).slice(text.split(/[, ]+/).indexOf('me'));
             text.shift();
             findGif(text.join('+'));
         }
     });
+    bot.addListener('notice', function (nick, to, text, message){
+        console.log('nick: ' + nick);
+        console.log('to: ' + to);
+        console.log('text: ' + text);
+        console.log('message: ' + message);
+    })
 }
 
 function findGif(search){
-    var url = 'http://api.giphy.com/v1/gifs/search?q=' + search + publicBetakey
+    var url = 'http://api.giphy.com/v1/gifs/random?tag=' + search + publicBetakey
     request(url, function (err, res, body){
         if(!err && res.statusCode == 200 && _.isEmpty(JSON.parse(body).data)!=true){
             body = JSON.parse(body);
             console.log(body);
-            bot.say(testRoom, body.data[randomize(body)].images.original.url);
+            bot.say(testRoom, body.data.image_original_url);
         }
         else{
             bot.say(testRoom, 'NOPE!');
         }
     })
 }
-function randomize(data){
-    var count = (data.pagination.count) - 1;
-    if (count >= 0){
-        var random = _.random(0, count);
-        console.log(random);
-        return random;
-    }
-}
+// function randomize(data){
+//     var count = (data.pagination.count) - 1;
+//     if (count >= 0){
+//         var random = _.random(0, count);
+//         console.log(random);
+//         return random;
+//     }
+// }
 
 init();
 
